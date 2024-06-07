e88fca65ff4504500bdf8e5a003f160c3a669365
Abraham Raies <41878814+abrahamraies@users.noreply.github.com>
2024-06-07 06:59:21 +0000

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abraham Raies",
+  title: "Software Developer",
+  email: "abrahamraies@gmail.com",
+  gitHub: "abrahamraies",
+  instagram: "abrahamraies",
+  linkedIn: "abrahamraies",
   medium: "",
-  twitter: "microsoft",
+  twitter: "abrahamraies",
   youTube: "Code",
 };
 
